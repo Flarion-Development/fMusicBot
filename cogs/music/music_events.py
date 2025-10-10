b87f00fe7a1a6d285b32b3715182835538a949c9
Alper Elieyi <alper.elieyi23@ogr.atauni.edu.tr>
2025-10-10 18:45:21 +0300

    fix

--- cogs/music/music_events.py
+++ cogs/music/music_events.py
@@ -22,10 +22,6 @@ class MusicEvents:
         self.command_channels = {}
         self.autoplay_enabled = {}
         self.played_songs = set()
-        self.host: str = host if host != None else "localhost"
-        self.port: int = port if port != None else 2333
-        self.password: str = password if password != None else "youshallnotpass"
-        self.secure: bool = secure if secure != None else False
     
 
     def get_youtube_music_recommendation(self,video_id: str):
@@ -58,11 +54,11 @@ class MusicEvents:
 
         try:
             await self.nodes.create_node(
-                host=self.host,
-                port=self.port,
+                host=host,
+                port=port,
                 label="MAIN",
-                password=self.password,
-                secure=self.secure
+                password=password,
+                secure=secure
             )
             logger.info("Connected to Lavalink node")
         except Exception as e:
